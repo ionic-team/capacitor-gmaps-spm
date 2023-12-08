19438c503ac6aded5658e53504204ce10f28d877
Mark Anderson <mark@ionic.io>
2023-12-08 12:55:21 -0500

    feat: inital commit of binary google maps package.swift

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "capacitor-gmaps-sdk",
+    products: [
+        .library(
+            name: "GoogleMaps",
+            targets: ["GoogleMaps"]
+        )
+        .library(
+            name: "GoogleMapsBase",
+            targets: ["GoogleMapsBase"]
+        )
+        .library(
+            name: "GoogleMapsCore",
+            targets: ["GoogleMapsCore"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "GoogleMaps",
+            url: "https://github.com/ionic-team/capacitor-gmaps-test/releases/download/8.3.1/GoogleMaps.xcframework.zip",
+            checksum: "4d9b8a4836d52eb7ef6257968853a9949b02cde299a05fe7389efab54c59128c"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsBase",
+            url: "https://github.com/ionic-team/capacitor-gmaps-test/releases/download/8.3.1/GoogleMapsBase.xcframework.zip",
+            checksum: "4e865a2f22e393f51546d4095452f4a93e067539a0c48339bb0f579fb5eb163a"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsCore",
+            url: "https://github.com/ionic-team/capacitor-gmaps-test/releases/download/8.3.1/GoogleMapsCore.xcframework.zip",
+            checksum: "31d11bdb0b12bac2bf33e3416c15bd09258a4e193fd8a5ceb452bad281c66626"
+        ),
+    ]
+)
