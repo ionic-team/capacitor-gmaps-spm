d078de9b3d3d76d234f58f38cab56feec2c378ab
Mark Anderson <mark@ionic.io>
2023-12-08 13:11:29 -0500

    fix: package.swift paths

--- Package.swift
+++ Package.swift
@@ -21,17 +21,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/ionic-team/capacitor-gmaps-test/releases/download/8.3.1/GoogleMaps.xcframework.zip",
+            url: "https://github.com/ionic-team/capacitor-gmaps-spm/releases/download/8.3.1/GoogleMaps.xcframework.zip",
             checksum: "4d9b8a4836d52eb7ef6257968853a9949b02cde299a05fe7389efab54c59128c"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/ionic-team/capacitor-gmaps-test/releases/download/8.3.1/GoogleMapsBase.xcframework.zip",
+            url: "https://github.com/ionic-team/capacitor-gmaps-spm/releases/download/8.3.1/GoogleMapsBase.xcframework.zip",
             checksum: "4e865a2f22e393f51546d4095452f4a93e067539a0c48339bb0f579fb5eb163a"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/ionic-team/capacitor-gmaps-test/releases/download/8.3.1/GoogleMapsCore.xcframework.zip",
+            url: "https://github.com/ionic-team/capacitor-gmaps-spm/releases/download/8.3.1/GoogleMapsCore.xcframework.zip",
             checksum: "31d11bdb0b12bac2bf33e3416c15bd09258a4e193fd8a5ceb452bad281c66626"
         ),
     ]
